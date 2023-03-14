278de7527d5e9ce1927b66553bdcdb6bbc7cedfa
Furkan Kovan <furkankovankaya@gmail.com>
2023-03-14 17:28:57 +0300

    add __name__ condition

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Channel.channel import Channel
 
-channel_name = input("Enter a Youtube channel name: ")
+if __name__ == '__main__':
+    channel_name = input("Enter a Youtube channel name: ")
 
-channel = Channel(channel_name)
-channel_url = channel.getUrl()
-channel_id = channel.getChannelId(channel_url)
-channel.getMainPageVideos(channel_url, channel_id)
+    channel = Channel(channel_name)
+    channel_url = channel.getUrl()
+    channel_id = channel.getChannelId(channel_url)
+    channel.getMainPageVideos(channel_url, channel_id)
